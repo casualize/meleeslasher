9b50d195ee3fa02576d881db588d4e21e412f2e8
casual <63418759+casualize@users.noreply.github.com>
2022-08-03 02:24:54 +0300

    post-full transistion to setdt

--- entities/weapons/weapon_ms_base/cl_init.lua
+++ entities/weapons/weapon_ms_base/cl_init.lua
@@ -1,5 +1,17 @@
 INC_CLIENT()
 
+function SWEP:SetupDataTables()
+	self:NetworkVar("Int", 0, "Got")
+	self:NetworkVarNotify("Got", function(name, old, new)
+		print(name, old, new)
+	end)
+end
+
 function SWEP:CalcViewModelView(vm, oP, oA)
 	return oP, oA
+end
+
+function SWEP:PrimaryAttack()
+	self.m_iState = STATE_ATTACK
+	self.m_iAnim = ANIM_STRIKE
 end
